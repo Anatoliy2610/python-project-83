517487f6aaff6ac46837412a927e8711390bbada
Anatoliy <anatoliy.klimanov@gmail.com>
2024-09-02 14:49:06 +0300

    исправления datatabase.sql

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
-drop table urls;
+-- drop table urls;
 CREATE TABLE IF NOT EXISTS urls (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL UNIQUE,
     created_at DATE NOT NULL
 );
 
-drop table url_checks;
+-- drop table url_checks;
 CREATE TABLE IF NOT EXISTS url_checks (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url_id BIGINT NOT NULL,
